73f81623536d25fcac79648c6b9813b2a0b87a3c
s-saito <s.s.saito108@gmail.com>
2021-04-24 15:07:02 +0900

    残りのコーディング完了

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,26 @@
       <div class="search-box">
         <input type="text" class="search-bar" placeholder="Search..."/>
       </div>
+
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">
+            Northampton, UK
+          </div>
+          <div class="date">
+            Monday 20 January 2020
+          </div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">
+            9°c
+          </div>
+          <div class="weather">
+            Rain
+          </div>
+        </div>
+      </div>
     </main>
   </div>
 </template>
@@ -72,5 +92,48 @@ export default {
     box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
     background-color: rgba(255, 255, 255, 0.75);
     border-radius: 16px 0px 16px 0px; 
+  }
+
+  .location-box .location {
+    color: #FFF;
+    font-size: 32px;
+    font-weight: 500;
+    text-align: center;
+    text-shadow: 1px 3px rgba(0, 0, 0, 0.25)
+  }
+
+  .location-box .date {
+    color: #FFF;
+    font-size: 20px;
+    font-weight: 300;
+    font-style: italic;
+    text-align: center;
+  }
+
+  .weather-box {
+    text-align: center;
+  }
+
+  .weather-box .temp {
+    display: inline-block;
+    padding: 10px 25px;
+    color: #FFF;
+    font-size: 102px;
+    font-weight: 900;
+
+    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+    background-color: rgba(255, 255, 255, 0.25);
+    border-radius: 16px;
+    margin: 30px 0px;
+
+    box-shadow: rgba(0, 0, 0, 0.25);
+  }
+
+  .weather-box .weather {
+    color: #FFF;
+    font-size: 48px;
+    font-weight: 700;
+    font-style: italic;
+    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
   }
 </style>
